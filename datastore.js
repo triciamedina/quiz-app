a3d4f6e11b22a4e3128f9f43ae55e5e24c45de45
Tricia Medina <tricia.medina@gmail.com>
2019-04-15 22:00:39 -0700

    add datastore.js

---
+++ datastore.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const STORE = [
+    {
+        question: "So, what is blockchain?",
+        options: [
+            "A company founded by Satoshi Nakamoto that created Bitcoin",
+            "An app that lets you purchase, sell, and mine various types of cryptocurrency",
+            "A distributed and public digital ledger that is used to record transactions across a network",
+            "A video game that allows players to purchase, collect, breed, and sell various types of virtual cats",
+        ],
+        answer: "A distributed and public digital ledger that is used to record transactions across a network",
+        explanation: "Blockchain technology is neither a company nor an app. At its most basic level, a blockchain is a growing list of records containing digital information (the “blocks”), which are linked using cryptography. Data contained within the blockchain is stored in a public database distributed across computers in a network so that any involved record cannot be altered retroactively, without the alteration of all subsequent blocks.",
+    },
+    {
+        question: "What kind of information can be recorded on a blockchain?",
+        options: [
+            "Monetary transactions",
+            "An agreement between two parties",
+            "How much electricity a lightbulb has used",
+            "All of the above",
+        ],
+        answer: "All of the above",
+        explanation: "The information recorded on a blockchain can take on any form, whether it be denoting a transfer of money, ownership, a transaction, someone’s identity, music, an agreement between two parties, or even how much electricity a lightbulb has used.",
+    },
+    {
+        question: "A blockchain database is managed using what kind of network?",
+        options: [
+            "Virtual private network",
+            "Peer-to-peer network",
+            "Point-to-point network",
+            "Local area network",
+        ],
+        answer: "Peer-to-peer network",
+        explanation: "Rather than keeping information in one central point, as is done by traditional recording methods, blockchain is managed by a peer-to-peer network. A peer-to-peer network is a decentralized and interconnected network that shares tasks or workloads—such as processing power or data storage—between all participants equally. What they create, store, or transfer is made available to everyone on the network.",
+    },
+    {
+        question: "What is a node?",
+        options: [
+            "A currency which is used for making or receiving payments on the blockchain",
+            "The initial block within a blockchain",
+            "A number used in a cryptographic communication",
+            "A device connected to a blockchain network",
+        ],
+        answer: "A device connected to a blockchain network",
+        explanation: "A node can be any active electronic device, including a computer, phone or even a printer, as long as it is connected to the internet and as such has an IP address. The role of a node is to support the network by maintaining a copy of a blockchain and, in some cases, to process transactions. All nodes are equal in importance on a blockchain, but a node can have different roles in making a blockchain work.",
+    },
+    {
+        question: "Which aspect of blockchain technology ensures that all nodes in a network are synchronized and agree on the validity of a transaction?",
+        options: [
+            "Consensus protocols",
+            "Smart contracts",
+            "Hash functions",
+            "Tokens",
+        ],
+        answer: "Consensus protocols",
+        explanation: "A consensus protocol is a set of rules that describe how the communication and transmission of data between electronic devices, such as nodes, works. Consensus is achieved when enough devices are in agreement about what is true and what should be recorded onto a blockchain. Therefore, consensus protocols are the governing rules that allow devices that are scattered across the world to factually come to an agreement, allowing a blockchain network to function without being corrupted."
+    },
+    {
+        question: "There are a variety of consensus protocols in blockchain. What is the consensus protocol introduced by Bitcoin?",
+        options: [
+            "Proof of Stake",
+            "Delegated Proof of Stake",
+            "Proof of Purchase",
+            "Proof of Work",
+        ],
+        answer: "Proof of Work",
+        explanation: "Proof of work is the consensus protocol introduced by Bitcoin and used widely by many other cryptocurrencies. The process is known as mining and the nodes on the network are known as “miners”. The “proof of work” comes in the form of an answer to a mathematical problem, one that requires considerable work to arrive at, but is easily verified to be correct once the answer has been reached. However, many regard proof of work as flawed and even unethical, since it has been reported that a single Bitcoin transaction, using proof of work, can consume as much electricity as an average Dutch household does in two weeks.",
+    },
+    {
+        question: "What is cryptography?",
+        options: [
+            "The study and practice of making maps",
+            "The science of arranging events in their order of occurrence in time",
+            "The study and practice of encrypting and decrypting information",
+            "The aspect of language study concerned with letters and their sequences in words",
+        ],
+        answer: "The study and practice of encrypting and decrypting information",
+        explanation: "Cryptography is the method of disguising and revealing, otherwise known as encrypting and decrypting information so that it can only be viewed by the intended recipients and nobody else. In modern terms, this is done through complex mathematics. The method involves taking unencrypted data, such as a piece of text, and encrypting it using a mathematical algorithm, known as a cipher.",
+    },
+    {
+        question: "What type of cryptography is used in blockchain?",
+        options: [
+            "Symmetric-key cryptography",
+            "Public-key cryptography",
+            "ROT13 Cipher",
+            "Enigma Cipher",
+        ],
+        answer: "Public-key cryptography",
+        explanation: "Public-key cryptography, or asymmetric cryptography, is a cryptographic system that uses pairs of keys: public keys which are used to encrypt and may be disseminated widely, and private keys which are used to decrypt and known only to the owner. This type of cryptography is considerably better suited to blockchain technology compared to symmetric-key cryptography because a user can send their public key to anyone without worrying that someone will gain access to their private key.",
+    },
+    {
+        question: "One of the key properties of blockchain is immutability. What gives blockchain this property?",
+        options: [
+            "Cryptographic hash functions",
+            "Remote procedure calls ",
+            "Mining",
+            "Genesis blocks",
+        ],
+        answer: "Cryptographic hash functions",
+        explanation: "Immutability, in the context of the blockchain, means that once something has been entered into the blockchain, it cannot be tampered with. The reason why the blockchain gets this property is that of cryptographic hash function. A cryptographic hash function is a hash function which takes an input (or “message”) and returns an alphanumeric string of a fixed-length. Changing any record that has previously happened on a blockchain would change all the hashes, making them false and obsolete.",
+    },
+    {
+        question: "Blockchain is often described as what?",
+        options: [
+            "Trustless",
+            "Trust-based",
+            "Centralized",
+            "Private",
+        ],
+        answer: "Trustless",
+        explanation: "Blockchain technology creates what is referred to as a trustless model because it does not require trust to safely interact and transact. In blockchain, cryptography completely replaces third parties as the governor of trust. Participants on the network run complex consensus protocols to unanimously and securely agree on what should be added to the distributed ledger of blockchain, while also ensuring its integrity at all times.",
+    },
+
+];
--- index.js
+++ index.js
@@ -1,49 +1,69 @@
+'use strict';
+
 function handleStartQuiz() {
     // The starting screen should have a button that users can click to start the quiz
     // The results screen should have a button that users can click to start quiz
+    // Upon starting quiz, current question is set to index 0
     // Upon clicking start quiz, the welcome screen/results screen is hidden
     // Upon clicking start quiz, the quiz is display beginning with the first question
+
+    console.log("`handleStartQuiz` ran");
 }
 
 // WILL NEED SOMETHING TO HOLD CURRENT QUESTION
 
 function handleRenderQuiz() {
     // This function displays the current question and choices
-    // At the beginning of the quiz users should be show the first question (index 0)
     // Users should also be able to see which question they're on (for instance, "7 out of 10")
-    // Users should also be able to see their current score ("5 correct, 2 incorrect").
+    // Users should also be able to see their current score ("5 correct, 2 incorrect")
+    // Users should be shown a submit button
+
+    console.log("`handleRenderQuiz` ran");
 }
 
 // WILL NEED SOMETHING TO HOLD CURRENT SCORE
 
 function handleSubmitAnswer() {
-    // Upon clicking submit the quiz is hidden and answer is rendered
     // Upon submitting a correct answer the score is incremented by 1
     // Upon submitting an incorrect answer the score is unchanged
+    // Upon clicking submit the quiz is hidden and answer is rendered
+
+    console.log("`handleSubmitAnswer` ran");
 }
 
 function handleIsAnswerTrueOrFalse () {
     // Upon submitting an answer, this function checks whether answer is correct and returns a value of true or false
+
+    console.log("`handleIsAnswerTrueOrFalse` ran");
 }
 
 function handleRenderAnswer() {
-    // Upon submitting an incorrect answer text feedback will display incorrect
-    // Upon submitting a correct answer text feedback will display correct
-    // Whether incorrect or correct, a text explanation of the correct answer is shown
-    // Whether incorrect or correct, users will be shown a button to move onto the next question
+    // If answer is false, text feedback will display incorrect
+    // If answer is true, text feedback will display correct
+    // Whether true or false, a text explanation of the correct answer is shown
+    // Whether whether true or false, users will be shown a button to move onto the next question
+
+    console.log("`handleRenderAnswer` ran");
 }
 
 function handleClickNextQuestion() {
-    // Upon clicking the next button quiz is displayed and user is shown the next question
+    // Upon clicking the next button current question is incremented by 1 
+    // Upon clicking the next button answer is hidden and quiz is displayed 
+
+    console.log("`handleClickNextQuestion` ran");
 }
 
 function handleRenderResults() {
     // Upon reaching the end of the quiz the quiz is hidden and results screen is shown
-    // Users should be shown their overall score at the end of the quiz
+    // Users should be shown their current score
+    // Depending on the users score range, text feedback is shown
     // Users should be shown a button to restart the quiz
+
+    console.log("`handleRenderResults` ran");
 }
 
 function handleQuiz() {
-    // This function will be the callback when the page loads
-    // It's responsible for activating the handleStartQuiz function
+    handleStartQuiz();
 }
+
+$(handleQuiz);
