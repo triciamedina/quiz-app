f9c4e938cd94bea234b47b41c8a6003eed88b245
Tricia Medina <tricia.medina@gmail.com>
2019-04-18 22:25:08 -0700

    update index.js with all functionality

--- index.js
+++ index.js
@@ -1,54 +1,51 @@
 'use strict';
 
-// Hold question count and score count
 let questionCount;
 let scoreCount;
 
 function handleStartQuiz() {
+    scoreCount = 0;
+    questionCount = 0;
 
     $(".js-start-quiz").on("click", function(event) {
-        
-        // Upon starting quiz, questionCount and scoreCount are set to 0
-        scoreCount = 0;
-        questionCount = 0;
-
-        // Hide current screen. Note – This also works: $(this).closest("section").hide();
-        $(this).closest("section").addClass("js-hide");
-
-        // Render quiz
-        handleRenderQuiz();
-
+        handleHideScreen();
+        handleRenderQuiz();  
         console.log("`handleStartQuiz` ran");
     });
 }
 
-function generateCurrentQuestion() {
+function handleHideScreen() {
+    // Note – This also works: $("section").hide();
+    // Animation can go here?
+    $("section").addClass("js-hide");
+    // $("section").slideUp();
+    console.log("`hideScreen` ran");
+}
 
-    // Looks up current question and stores question and options HTML
-    const currentQuestion = STORE[questionCount];
+function generateCurrentQuestion() {
     return `<section class="container">
             <header>
                 <span id="score">Score: ${scoreCount}</span>
-                <h2>Question ${currentQuestion.number} of 10</h2>
+                <h2>Question ${STORE[questionCount].number} of 10</h2>
             </header>
             <form id="js-quiz-form">
             <fieldset>
-                <legend class="question">${currentQuestion.question}</legend>
+                <legend class="question">${STORE[questionCount].question}</legend>
                 <div class="radio-container">
-                    <input type="radio" name="question" id="answer-1" value="0" required>
-                    <label for="answer-1">${currentQuestion.options[0]}</label>
+                    <input type="radio" name="question" id="answer-1" value="${STORE[questionCount].options[0]}" required>
+                    <label for="answer-1">${STORE[questionCount].options[0]}</label>
                 </div>
                 <div class="radio-container">
-                    <input type="radio" name="question" id="answer-2" value="1" required>
-                    <label for="answer-2">${currentQuestion.options[1]}</label>
+                    <input type="radio" name="question" id="answer-2" value="${STORE[questionCount].options[1]}" required>
+                    <label for="answer-2">${STORE[questionCount].options[1]}</label>
                 </div>
                 <div class="radio-container">
-                    <input type="radio" name="question" id="answer-3" value="1" required>
-                    <label for="answer-3">${currentQuestion.options[2]}</label>
+                    <input type="radio" name="question" id="answer-3" value="${STORE[questionCount].options[2]}" required>
+                    <label for="answer-3">${STORE[questionCount].options[2]}</label>
                 </div>
                 <div class="radio-container">
-                    <input type="radio" name="question" id="answer-4" value="1" required>
-                    <label for="answer-4">${currentQuestion.options[3]}</label>
+                    <input type="radio" name="question" id="answer-4" value="${STORE[questionCount].options[3]}" required>
+                    <label for="answer-4">${STORE[questionCount].options[3]}</label>
                 </div>
             </fieldset>
             <button type="submit" role="button" class="js-submit-answer">Submit</button>
@@ -57,109 +54,128 @@ function generateCurrentQuestion() {
 }
 
 function handleRenderQuiz() {
-
-    // Renders current question and choices in the DOM
-    const quizElement = generateCurrentQuestion();
+    let quizElement = generateCurrentQuestion();
+    // Animation can go here?
     $("body").prepend(quizElement);
-
     handleSubmitAnswer();
     console.log("`handleRenderQuiz` ran");
 }
 
+function handleUpdateScoreCount() {
+    scoreCount++;
+}
+
 function handleSubmitAnswer() {
-
     $("#js-quiz-form").submit(function(event) {
+        let selected = $("input:checked").val();
+        let correctAnswer = STORE[questionCount].answer;
 
-        // If true increment score by 1
-        if (handleIsAnswerTrueOrFalse() == true) {
-            scoreCount++;
+        if (selected === correctAnswer) {
+            handleUpdateScoreCount();
+            handleHideScreen();
+            renderAnswerCorrect();
+            console.log('answer is correct');
+        } else {
+            handleHideScreen();
+            renderAnswerWrong();
+            console.log("answer is wrong");
         }
-       
-        // Hide quiz. Note – This also works: $(this).closest("section").hide();
-        $(this).closest("section").addClass("js-hide");
-
-        // Display answer
-        handleRenderAnswer();
 
         event.preventDefault();
         console.log("Button works!");
     });
-    
     console.log("`handleSubmitAnswer` ran");
 }
 
-function handleIsAnswerTrueOrFalse () {
-    // Get the value of the option submitted and compare to currentQuestion.answer
-
-    console.log("`handleIsAnswerTrueOrFalse` ran");
-}
-
-function generateCurrentAnswer() {
-    // Looks up current question and stores explanation in HTML
-    const currentQuestion = STORE[questionCount];
-    return `<section class="container">
-                <header>
-                    <h2>Score: 1</h2>
-                    <div class="result">That&rsquo;s Right!</div>
-                </header>
-                <div class="correct-answer">
-                    <p>${currentQuestion.explanation}</p>
-                </div>    
-                <button role="button" class="js-next-question">Next</button>
-            </section>`
-}
-
-function handleRenderAnswer() {
-    // Renders answer and next button in DOM
-    const answerElement = generateCurrentAnswer();
+function renderAnswerCorrect() {
+    let answerElement = 
+    `<section class="container">
+        <header>
+            <h2>Score: ${scoreCount}</h2>
+            <div class="result">That's Right!</div>
+        </header>
+        <div class="correct-answer">
+            <p>${STORE[questionCount].explanation}</p>
+        </div>    
+        <button role="button" class="js-next-question">Next</button>
+    </section>`
+    
     $("body").prepend(answerElement);
-
-    // If answer is false, text feedback will display incorrect
-    // If answer is true, text feedback will display correct
-
     handleClickNextQuestion();
     console.log("`handleRenderAnswer` ran");
 }
 
+function renderAnswerWrong() {
+    let answerElement = 
+    `<section class="container">
+        <header>
+            <h2>Score: ${scoreCount}</h2>
+            <div class="result">That's Wrong!</div>
+        </header>
+        <div class="correct-answer">
+            <p>${STORE[questionCount].explanation}</p>
+        </div>    
+        <button role="button" class="js-next-question">Next</button>
+    </section>`
+    
+    $("body").prepend(answerElement);
+    handleClickNextQuestion();
+    console.log("`handleRenderAnswer` ran");
+}
+
+function updateQuestionCount() {
+    questionCount++;
+}
+
 function handleClickNextQuestion() {
-
     $(".js-next-question").on("click", function(event) {
-        // Current question is incremented by 1 
-        questionCount++;
-
-        // Hide answer screen. Note – This also works: $(this).closest("section").hide();
-        $(this).closest("section").addClass("js-hide");
+        updateQuestionCount();
+        handleHideScreen();
 
         if (questionCount < STORE.length) {
-            // Display quiz
             handleRenderQuiz();  
         } else {
-            // Display results
-            handleRenderResults();
+            handleRenderQuizResults();
         };
     })
-
     console.log("`handleClickNextQuestion` ran");
 }
 
-function generateResults() {
-    return `<section class="container">
+function generateQuizResults() {
+    if (scoreCount >= 8) {
+        return `<section class="container">
                 <header>
                     <h1>Nice Job.</h1>
-                    <h2>You scored 10 out of 10</h2>
+                    <h2>You scored ${scoreCount} out of 10</h2>
                 </header>
-                <p>Congrats! You know your stuff. Now get out there and blockchain responsibly. </p>
+                <p>Congrats! You know your stuff. Now get out there and blockchain responsibly.</p>
                 <button role="button" class="js-start-quiz">Try Again</button>
-            </section>`
+            </section>`;
+    } else if (scoreCount >= 5) {
+        return `<section class="container">
+                <header>
+                    <h1>Not Bad.</h1>
+                    <h2>You scored ${scoreCount} out of 10</h2>
+                </header>
+                <p>You have a good foundation but there is more to learn.</p>
+                <button role="button" class="js-start-quiz">Try Again</button>
+            </section>`;
+    } else {
+        return `<section class="container">
+                <header>
+                    <h1>Keep Studying.</h1>
+                    <h2>You scored ${scoreCount} out of 10</h2>
+                </header>
+                <p>Take the quiz again to see if you can do better.</p>
+                <button role="button" class="js-start-quiz">Try Again</button>
+            </section>`;
+    };
+    
 }
 
-function handleRenderResults() {
-    const resultsElement = generateResults();
+function handleRenderQuizResults() {
+    let resultsElement = generateQuizResults();
     $("body").prepend(resultsElement);
-    
-    // Users should be shown their current score
-    // Depending on the users score range, text feedback is shown
-
     handleStartQuiz();
     console.log("`handleRenderResults` ran");
 }
